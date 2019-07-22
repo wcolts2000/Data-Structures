71160faa3bf3a02e77f940203932c2ffdb33bae9
wcolts2000 <wcolts2000@gmail.com>
2019-07-22 12:24:48 -0700

    completed queue class

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,37 @@
-class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+class Queue:  # FIFO
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        new_node = Node(item)
+        if not self.first:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
 
-  def len(self):
-    pass
+        self.size += 1
+
+    def dequeue(self):
+        if not self.first:
+            return None
+        if self.first == self.last:
+            self.last = None
+        temp = self.first
+        self.first = self.first.next
+        self.size -= 1
+        return temp.value
+
+    def len(self):
+        return self.size
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
