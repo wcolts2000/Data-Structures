5b6938af0a18ec768fc193746a4c926aeaab1a39
wcolts2000 <wcolts2000@gmail.com>
2019-07-24 15:11:58 -0700

    heap tests passing

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -2,12 +2,23 @@ class Heap:
     def __init__(self):
         self.storage = []
 
+    def __str__(self):
+        return str(self.storage)
+
     def insert(self, value):
         self.storage.append(value)
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        # swap first index with last index
+        self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+
+        # capture popped end of array
+        removed_val = self.storage.pop()
+        # sift down index 0
+        self._sift_down(0)
+        # return removed capture
+        return removed_val
 
     def get_max(self):
         return self.storage[0]
@@ -33,26 +44,34 @@ class Heap:
                 break
 
     def _sift_down(self, index):
-        pass
+        # declare max index with arr length
+        list_length = len(self.storage) - 1
 
+        # shortened variable name for clarity
+        arr = self.storage
 
-'''
-- Should have the methods `insert`, `delete`, `get_max`, `_bubble_up`, and `_sift_down`.
-  - `insert` adds the input value into the heap; this method should ensure that the inserted value is in the correct spot in the heap
-  - `delete` removes and returns the 'topmost' value from the heap; this method needs to ensure that the heap property is maintained after the topmost element has been removed.
-  - `get_max` returns the maximum value in the heap _in constant time_.
-  - `get_size` returns the number of elements stored in the heap.
-  - `_bubble_up` moves the element at the specified index "up" the heap by swapping it with its parent if the parent's value is less than the value at the specified index.
-  - `_sift_down` grabs the indices of this element's children and determines which child has a larger value. If the larger child's value is larger than the parent's value, the child element is swapped with the parent.
-'''
-# heap = Heap()
+        # while there is the chance of a right child
+        while 2 * index + 2 <= list_length:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            # declare changing variables for while loop
+            # if the left child is bigger than the right child
+            if arr[left] >= arr[right]:
+                # swap left with child and index
+                if arr[index] <= arr[left]:
+                    self.swap(index, left)
+                    # set new index location to check if values there need swapping
+                    index = left
+            # if right is larger than left child
+            elif arr[left] <= arr[right]:
+                # and right is larger than index locations value
+                if arr[index] <= arr[right]:
+                    self.swap(index, right)
+                    # swap and reassign index
+                    index = right
+            # otherwise break out
+            else:
+                return
 
-# heap.insert(6)
-# heap.insert(8)
-# heap.insert(10)
-# heap.insert(9)
-# heap.insert(1)
-# heap.insert(9)
-# heap.insert(9)
-# heap.insert(5)
-# print(heap.storage, [10, 9, 9, 6, 1, 8, 9, 5])
+    def swap(self, index1, index2):
+        self.storage[index1], self.storage[index2] = self.storage[index2], self.storage[index1]
